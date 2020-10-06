742cfc18d8d4048d53c5c912d36800459bea0171
Daniyal Kabir Dar <71030497+daniyalkabir@users.noreply.github.com>
2020-10-06 08:35:27 +0530

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Akash_DSU on 1/31/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
